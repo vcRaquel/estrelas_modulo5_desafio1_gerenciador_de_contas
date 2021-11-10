7e4273541c5a377918623a77f11bb92f52d9e880
Raquel Leão <vc.raquel@gmail.com>
2021-11-10 16:10:51 -0300

    colocar injecao de dependencias na controller

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaController.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaController.java
@@ -1,10 +1,16 @@
 package br.com.zup.gerenciador_de_contas.contas;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
+    @Autowired
+    private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
 
 }
