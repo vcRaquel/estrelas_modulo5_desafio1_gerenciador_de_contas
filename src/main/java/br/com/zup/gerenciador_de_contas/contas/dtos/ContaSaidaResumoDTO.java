42a806881ca646979ecb9d8b05aadbe416b32e48
Raquel Leão <vc.raquel@gmail.com>
2021-11-10 18:14:35 -0300

    terminar de criar classe ContaSaidaResumoDTO para retorno da lista de contas armazenadas

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/dtos/ContaSaidaResumoDTO.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/dtos/ContaSaidaResumoDTO.java
@@ -7,4 +7,39 @@ public class ContaSaidaResumoDTO {
     private String nome;
     private double valor;
     private Status status;
+
+    public ContaSaidaResumoDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
