899bfd20d7d5add3109e7b0d548f9f78d45365dd
Raquel Leão <vc.raquel@gmail.com>
2021-11-10 15:14:05 -0300

    criar classe Conta que será a entidade

---
+++ src/main/java/br/com/zup/gerenciador_de_contas/Conta.java
@@ -0,0 +1,81 @@
+package br.com.zup.gerenciador_de_contas;
+
+import br.com.zup.gerenciador_de_contas.enuns.Tipo;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="contas")
+public class Conta {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(nullable = false)
+    private Tipo tipo;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private double valor;
+
+    @Column(nullable = false)
+    private LocalDate dataDeVencimento;
+
+    @Column(nullable = true)
+    private LocalDateTime dataDePagamento;
+
+    public Conta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+}
