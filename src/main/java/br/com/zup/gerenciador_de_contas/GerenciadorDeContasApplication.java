e0adaeb11214f9edc1341fa4997354bac0cae533
Raquel Leão <vc.raquel@gmail.com>
2021-11-10 14:23:44 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/gerenciador_de_contas/GerenciadorDeContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciador_de_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/gerenciador_de_contas/GerenciadorDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciador_de_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
