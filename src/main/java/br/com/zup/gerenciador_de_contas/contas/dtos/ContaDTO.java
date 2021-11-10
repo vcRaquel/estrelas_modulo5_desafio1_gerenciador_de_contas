b0690b6cb4078cfb56a6fdb311a57a2d9f7749a0
Raquel Leão <vc.raquel@gmail.com>
2021-11-10 15:37:12 -0300

    criar classe ContaDTO para entrada de dados

---
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/dtos/ContaDTO.java
@@ -0,0 +1,47 @@
+package br.com.zup.gerenciador_de_contas.contas.dtos;
+
+import br.com.zup.gerenciador_de_contas.enuns.Tipo;
+
+import java.time.LocalDate;
+
+public class ContaDTO {
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+
+    public ContaDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+}
