2955c704fb9757cc8b2e734ddf87d25259083a58
Raquel Leão <vc.raquel@gmail.com>
2021-11-11 14:46:37 -0300

    manipular exception de validacao na controller advice

--- src/main/java/br/com/zup/gerenciador_de_contas/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/config/ControllerAdvisor.java
@@ -1,8 +1,28 @@
 package br.com.zup.gerenciador_de_contas.config;
 
+import br.com.zup.gerenciador_de_contas.MensagemDeErro;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvisor {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<MensagemDeErro> manipularNotValidExceptions(MethodArgumentNotValidException exception) {
+        List<MensagemDeErro> mensagens = new ArrayList<>();
+
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            mensagens.add(new MensagemDeErro(fieldError.getDefaultMessage()));
+        }
+        return mensagens;
+    }
 }
+
