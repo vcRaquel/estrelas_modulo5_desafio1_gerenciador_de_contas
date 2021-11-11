66424e06f95d04133f54ad7d7a0ce3d4c98a6489
Raquel Leão <vc.raquel@gmail.com>
2021-11-11 15:43:12 -0300

    validar se o status colocado é PAGO

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/service/ContaService.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/service/ContaService.java
@@ -1,6 +1,9 @@
-package br.com.zup.gerenciador_de_contas.contas;
+package br.com.zup.gerenciador_de_contas.contas.service;
 
+import br.com.zup.gerenciador_de_contas.contas.Conta;
+import br.com.zup.gerenciador_de_contas.contas.ContaRepository;
 import br.com.zup.gerenciador_de_contas.enuns.Status;
+import br.com.zup.gerenciador_de_contas.exceptionsPernonalizadas.StatusInvalidoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,29 +21,32 @@ public class ContaService {
 
         if (conta.getDataDeVencimento().isBefore(LocalDate.now())) {
             conta.setStatus(Status.VENCIDA);
-        }else{
+        } else {
             conta.setStatus(Status.AGUARDANDO);
         }
         return contaRepository.save(conta);
     }
 
-    public List<Conta> exibirContasCadastradas(){
+    public List<Conta> exibirContasCadastradas() {
         Iterable<Conta> contas = contaRepository.findAll();
         return (List<Conta>) contas;
     }
 
-    public Conta retornarContaPorId(int id){
-      Optional<Conta> conta = contaRepository.findById(id);
+    public Conta retornarContaPorId(int id) {
+        Optional<Conta> conta = contaRepository.findById(id);
         return conta.get();
         //e se não tiver o id?
     }
 
-    public Conta atualizarConta(int id){
+    public Conta atualizarConta(int id) {
         Conta conta = retornarContaPorId(id);
-        conta.setDataDePagamento(LocalDateTime.now());
         //e se o Status colocado pelo Put for diferente de Pago?
-        conta.setStatus(Status.PAGO);
-        contaRepository.save(conta);
-        return conta;
+        if (conta.getStatus().equals(Status.PAGO)) {
+            conta.setDataDePagamento(LocalDateTime.now());
+            conta.setStatus(Status.PAGO);
+            contaRepository.save(conta);
+            return conta;
+        }
+        throw new StatusInvalidoException("Status inválido para a requisição");
     }
 }
