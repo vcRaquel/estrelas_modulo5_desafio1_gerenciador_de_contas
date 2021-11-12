fd7ada8f81300a1e7aab074ddf973dc957437445
Raquel Leão <vc.raquel@gmail.com>
2021-11-11 22:54:31 -0300

    ajustar estimativa de valor aproximado

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
@@ -11,7 +11,8 @@ public interface ContaRepository extends CrudRepository <Conta, Integer>{
     List<Conta> findAllByStatus(Status status);
     List<Conta> findAllByTipo(Tipo tipo);
 
-    @Query(value = "SELECT * FROM contas WHERE VALOR BETWEEN :valor*0.9 AND :valor*1.2", nativeQuery = true)
+//    @Query(value = "SELECT * FROM contas WHERE VALOR BETWEEN :valor*0.9 AND :valor*1.2", nativeQuery = true)
+    @Query(value = "SELECT * FROM contas WHERE VALOR BETWEEN :valor-10 AND :valor+10", nativeQuery = true)
     List<Conta>findAllByValor(Double valor);
 
 }
