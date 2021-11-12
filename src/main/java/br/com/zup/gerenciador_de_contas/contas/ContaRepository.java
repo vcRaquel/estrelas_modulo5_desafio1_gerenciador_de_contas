7705c0c7e94d0ecb282a725e7a386c2e067fe393
Raquel Leão <vc.raquel@gmail.com>
2021-11-11 22:42:16 -0300

    adicionar filtro de valores aproximados na repository

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
@@ -2,6 +2,7 @@ package br.com.zup.gerenciador_de_contas.contas;
 
 import br.com.zup.gerenciador_de_contas.enuns.Status;
 import br.com.zup.gerenciador_de_contas.enuns.Tipo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.List;
 public interface ContaRepository extends CrudRepository <Conta, Integer>{
     List<Conta> findAllByStatus(Status status);
     List<Conta> findAllByTipo(Tipo tipo);
+
+    @Query(value = "SELECT * FROM contas WHERE VALOR BETWEEN :valor*0.9 AND :valor*1.2", nativeQuery = true)
+    List<Conta>findAllByValor(Double valor);
 
 }
 
