10145fc887065d885f9db50bd6c0f02be2980b3e
Raquel Leão <vc.raquel@gmail.com>
2021-11-11 21:54:17 -0300

    adicionar filtro para Status no metodo exibirContas na controller

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
@@ -1,12 +1,15 @@
 package br.com.zup.gerenciador_de_contas.contas;
 
 import br.com.zup.gerenciador_de_contas.enuns.Status;
+import br.com.zup.gerenciador_de_contas.enuns.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ContaRepository extends CrudRepository <Conta, Integer>{
     List<Conta> findAllByStatus(Status status);
+    List<Conta> findAllByTipo(Tipo tipo);
+
 }
 
 
