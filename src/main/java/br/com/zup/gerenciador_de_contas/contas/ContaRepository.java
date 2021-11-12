ed118fff7073195dd20b548eee1cf76eb2df0165
Raquel Leão <vc.raquel@gmail.com>
2021-11-11 21:48:26 -0300

    criar metodo de filtro para status na repository

--- src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
+++ src/main/java/br/com/zup/gerenciador_de_contas/contas/ContaRepository.java
@@ -1,8 +1,12 @@
 package br.com.zup.gerenciador_de_contas.contas;
 
+import br.com.zup.gerenciador_de_contas.enuns.Status;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ContaRepository extends CrudRepository <Conta, Integer>{
+    List<Conta> findAllByStatus(Status status);
 }
 
 
